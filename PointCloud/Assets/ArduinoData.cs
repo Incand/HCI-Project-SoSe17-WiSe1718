318dddefaa5028a31aca8774b4982b824a6dba24
HCI Lab <hci-wimi@informatik.uni-hamburg.de>
2017-05-08 15:44:27 +0200

    raycast

---
+++ PointCloud/Assets/ArduinoData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+public class ArduinoData : MonoBehaviour {
+    System.IO.StreamReader reader;
+    FileStream stream;
+    string path = "D:/Benutzer/MCIProject_FSH/data.txt";
+    // Use this for initialization
+    void Start () {
+        stream = new FileStream(path,FileMode.Open,FileAccess.Read);
+        reader = new StreamReader(stream);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(reader.ReadLine().Last());
+    }
+    void OnApplicationQuit()
+    {
+        stream.Close();
+        reader.Close();
+    }
+}
