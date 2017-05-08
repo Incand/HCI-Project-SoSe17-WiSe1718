91988b42f5b509d978ab955855315af58afe5987
HCI Lab <hci-wimi@informatik.uni-hamburg.de>
2017-05-08 18:09:59 +0200

    winkelüberarbeitung 2

--- PointCloud/Assets/Raycast/Raycast_Scripts/DataContainer.cs
+++ PointCloud/Assets/Raycast/Raycast_Scripts/DataContainer.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "DataContainer")]
 public class DataContainer : ScriptableObject {
 
-    public float frequencyVertical,frequencyHorizontal;
-    public float height;
-    public float width;
-    public float distance;
-
+	public float frequencyVertical,frequencyHorizontal;
+	public float verticalDegree;
+	public float horizontalDegree;
+	public float distance;
+	public float VerticalRadian {
+		get { return verticalDegree * Mathf.Deg2Rad; }
+	}
+	public float HorizontalRadian {
+		get { return horizontalDegree * Mathf.Deg2Rad; }
+	}
 }
--- PointCloud/Assets/Raycast/Raycast_Scripts/MoveRaycast.cs
+++ PointCloud/Assets/Raycast/Raycast_Scripts/MoveRaycast.cs
@@ -2,53 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveRaycast : MonoBehaviour {
+public class MoveRaycast : MonoBehaviour
+{
 
     [SerializeField]
     private DataContainer dc;
     private Vector3 origin, direction;
-    GameObject sphere, sphereHeight, sphereWidth;
+    GameObject sphere, sphereHeight, sphereWidth, sphereWidth2;
     private float amplitudeVertical, amplitudeHorizontal;
     private float frequencyVertical, frequencyHorizontal, distance;
     RaycastHit hit;
     FeedBackAudio fb;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         origin = gameObject.transform.position;
         sphere = GameObject.Find("Sphere");
         sphereHeight = GameObject.Find("SphereLimitHeight");
         sphereWidth = GameObject.Find("SphereLimitWidth");
+        sphereWidth2 = GameObject.Find("SphereLimitWidth2");
+
         fb = GetComponent<FeedBackAudio>();
         distance = dc.distance;
-        amplitudeVertical = dc.width / 2;
-        amplitudeHorizontal = dc.height / 2;
-        frequencyVertical =dc.frequencyVertical;
+        frequencyVertical = dc.frequencyVertical;
         frequencyHorizontal = dc.frequencyHorizontal;
         direction = new Vector3(0, 0, distance);
-        sphereHeight.transform.position = new Vector3(direction.x,amplitudeVertical, direction.z);
+        sphereHeight.transform.position = new Vector3(direction.x, amplitudeVertical, direction.z);
         sphereWidth.transform.position = new Vector3(amplitudeHorizontal, direction.y, direction.z);
+        sphereWidth2.transform.position = new Vector3(-1 * amplitudeHorizontal, direction.y, direction.z);
 
     }
 
     // Update is called once per frame
-    void Update () {
-        direction = getDirection();
-        bool treffer=Physics.Raycast(origin, direction, out hit,distance);
+    void Update()
+    {
+        direction = transform.position - direction;
+        bool treffer = Physics.Raycast(origin, direction, out hit, distance);
         Debug.Log(hit.distance);
         if (treffer) hitting();
         else nohit();
-        Debug.DrawRay(origin, direction,  Color.black, 0.01f);
-}
-    float getSineValue(float frequency,float amplitude)
+        Debug.DrawRay(origin, direction, Color.black, 0.01f);
+    }
+
+    float getSineValue(float frequency, float amplitude)
     {
         //sin x;
-        return amplitude*Mathf.Sin(frequency* (Time.time / (2 * Mathf.PI)));
+        return amplitude * Mathf.Sin(frequency * (Time.time / (2 * Mathf.PI)));
     }
     void hitting()
     {
         sphere.transform.position = hit.point;
-        fb.play(distance-hit.distance);
+        fb.play(distance - hit.distance);
     }
     void nohit()
     {
@@ -58,8 +63,8 @@ public class MoveRaycast : MonoBehaviour {
     }
     private Vector3 getDirection()
     {
-        direction.y = getSineValue(frequencyVertical,amplitudeVertical);
-        direction.x = getSineValue(frequencyHorizontal,amplitudeHorizontal);
+        direction.y = getSineValue(frequencyVertical, dc.VerticalRadian);
+        direction.x = getSineValue(frequencyHorizontal, dc.HorizontalRadian);
         return direction;
     }
 }
