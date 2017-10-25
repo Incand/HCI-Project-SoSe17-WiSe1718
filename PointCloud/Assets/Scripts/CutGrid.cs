133ba6e4689d62650366bd0111f9a70caaacc6c2
Orpartlu <3Somma@inf>
2017-10-25 11:02:38 +0200

    Cut the grid over a size of 2.2 meters

---
+++ PointCloud/Assets/Scripts/CutGrid.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TrapezeGrid;
+using UnityEngine;
+
+public class CutGrid : MonoBehaviour {
+
+    //Add script to the Feedback Loop, if cut yields true, then there is no Feedback neccessary.
+    //Potential problem : Sensor might be angled already 
+    public GridData griddata;
+	// Use this for initialization
+	void Start () {
+        //getAngle from IMU
+        //getDistance from Sensor
+	}
+	public bool cut()
+    {
+        return (calculateGradient() * distance >2.2f);  
+    }
+    public float calculateGradient()
+    {
+        return (angle<90.0f) ? Mathf.Tan(angle) : -1.0f* Mathf.Tan(180.0f-angle);
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
