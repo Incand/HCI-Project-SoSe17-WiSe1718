85e460803863404fac84e3f9a0aa68488f747251
Orpartlu <3Somma@inf>
2017-10-25 11:46:14 +0200

    minor fixes on Cutting Feedback

--- PointCloud/Assets/Scripts/CutGrid.cs
+++ PointCloud/Assets/Scripts/CutGrid.cs
@@ -6,16 +6,26 @@ using UnityEngine;
 public class CutGrid : MonoBehaviour {
 
     //Add script to the Feedback Loop, if cut yields true, then there is no Feedback neccessary.
+    // Use Case: Ground and rooms/objects above 2.2m
     //Potential problem : Sensor might be angled already 
-    public GridData griddata;
-	// Use this for initialization
+    float distance = 2.0f;
+    float angle = 90.0f;
+        // Use this for initialization
 	void Start () {
         //getAngle from IMU
         //getDistance from Sensor
+
+        //cut the ground, too
+        //height of Sensor and angle
 	}
 	public bool cut()
     {
-        return (calculateGradient() * distance >2.2f);  
+        //TODO: substract height from calculateIMU rotation to manage different cone rotations.
+        // check whether sensor data exceeds relevant object detection heights. 
+        if (angle > 0)
+            return (calculateGradient() * distance > 2.2f); // over-head
+        else
+            return (calculateGradient() * distance < 0.1f); // ground
     }
     public float calculateGradient()
     {
