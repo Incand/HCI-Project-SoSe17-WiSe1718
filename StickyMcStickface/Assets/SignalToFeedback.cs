f30cc9aa545a1f3ddd3ab997f64692953ba2af11
Orpartlu <3Somma@inf>
2017-11-08 14:07:29 +0100

    Added sonic sensor feedback

--- StickyMcStickface/Assets/SignalToFeedback.cs
+++ StickyMcStickface/Assets/SignalToFeedback.cs
@@ -6,6 +6,16 @@ public class SignalToFeedback : MonoBehaviour {
     public HapStickController hapcon;
     // Use this for initialization
 
+    [Header("Sonic Signal Processing")]
+    [SerializeField]
+    [Range(10.0f, 100.0f)]
+    private float _minSonicSignal = 20.0f;
+
+    [SerializeField]
+    [Range(100.0f, 600.0f)]
+    private float _maxSonicSignal = 200.0f;
+
+    [Header("Infrared Signal Processing")]
     [Range(10.0f, 100.0f)]
     [SerializeField]
     private float _minSignal = 20.0f;
@@ -18,15 +28,25 @@ public class SignalToFeedback : MonoBehaviour {
     private float mean = 0;
 
 	void Start () {
-		
+        IEnumerator corout = _sonicFeedbackCoroutine();
+        StartCoroutine(corout);
 	}
 	
+    private IEnumerator _sonicFeedbackCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.25f);
+            hapcon.durationMS = 250;
+            hapcon.amplitud = SonicSignalFeedback;
+            hapcon.triggerPiezo(true);
+            Debug.Log("Feedback!");
+        }
+    }
+    
+
 	// Update is called once per frame
 	void FixedUpdate () {
-        float signal = CombineMean(hapcon.LaserSensorDistance, hapcon.UltrasonicSensorDistance);
-        mean = signal;
-        hapcon.amplitud = calculateAmplitude(signal);
-
 
 	}
 
@@ -35,13 +55,23 @@ public class SignalToFeedback : MonoBehaviour {
     {
         return (laserDistance + sonicDistance) * 0.5f;
     }
+
+    private byte SonicSignalFeedback
+    {
+        get
+        {
+            float clampedSignal = Mathf.Clamp(hapcon.UltrasonicSensorDistance, _minSonicSignal, _maxSonicSignal);
+            float cSNorm = 1.0f - (clampedSignal - _minSignal) / (_maxSignal - _minSignal);
+            return (byte)(255 * cSNorm);
+        }
+    }
     #endregion
 
     #region FEEDBACK_CALCULATION
     private byte calculateAmplitude(float signal)
     {
         float clampedSignal = Mathf.Clamp(signal, _minSignal, _maxSignal);
-        float cSNorm = 1.0f - (signal - _minSignal) / (_maxSignal - _minSignal);
+        float cSNorm = 1.0f - (clampedSignal - _minSignal) / (_maxSignal - _minSignal);
         return (byte)(255 * cSNorm);
     }
     #endregion
