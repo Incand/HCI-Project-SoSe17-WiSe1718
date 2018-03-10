43f85d98bbdafc805732e5a747bac3ef2ba61c91
Armin <armin-scha@hotmail.de>
2018-03-10 20:12:44 +0100

    Added template for IMUDataGenerator

---
+++ StickyMcStickface/Assets/Scripts/IMUDataGeneration/IMUDataGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IMUDataGenerator : MonoBehaviour {
+
+	[SerializeField]
+	private Transform _imu;
+
+	[SerializeField]
+	private uint _participantId = 0;
+
+	[SerializeField]
+	private uint _courseId = 0;
+
+	[SerializeField]
+	private string _dataPath;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
